ad034ce230d8a4afd7a3ec07870e5b4b1fe4c659
PalmyRemember <Palmy_kiss8@hotmail.com>
2018-10-18 11:51:51 +0700

    init class Hello

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	
+String helloname;
+
+public String getHelloname() {
+	return helloname;
+}
+
+public void setHelloname(String helloname) {
+	this.helloname = helloname;
+}
+
+
+
+
+
+}
